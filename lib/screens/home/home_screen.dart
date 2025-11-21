92cf24c57df9799cfca22752bcac5ce37b37bd41
Azka Syakirah <azkasyakir800@gmail.comA>
2025-11-21 15:52:09 +0700

    finishing home screen UI definition

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:attendance_app/models/attendance_record.dart';
+import 'package:attendance_app/screens/home/widgets/action_button.dart';
 import 'package:attendance_app/screens/home/widgets/attendance_card.dart';
 import 'package:attendance_app/screens/home/widgets/profile_card.dart';
 import 'package:attendance_app/services/auth_services.dart';
@@ -180,7 +181,14 @@ class _HomeScreenState extends State<HomeScreen> {
               SizedBox(height: 24),
               AttendanceCard(todayRecord: _todayRecord),
               SizedBox(height: 24),
-              ActionsButton
+              ActionButton(
+                todayRecord: _todayRecord,
+                isLoading: _isLoading,
+                onCheckIn: () => _checkIn(),
+                onCheckOut: () => _checkOut(),
+                onCheckInWithPhoto: (path) => _checkIn(photoPath: path),
+                onCheckOutWithPhoto: (path) => _checkOut(photoPath: path),
+              )
             ],
           ),
         ),
